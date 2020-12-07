8da81b1e022b75e0632f3666266888e7c2800361
Albert <wi18b088@technikum-wien.at>
2020-12-07 14:13:15 +0100

    added gitignore, updated cyper

--- eot/wowool/samples/CypherToNeo4j.py
+++ eot/wowool/samples/CypherToNeo4j.py
@@ -1,7 +1,10 @@
 import neo4j
-neo4jdb = neo4j.Connector("http://localhost:7474", ("neo4j", "12345")) # local password of neo4j database
-with open("cypher-out.cypher") as fh:
+import os
+# print(os.listdir())
+# neo4jdb = neo4j.Connector("http://localhost:7474", ("neo4j", "12345")) # local password of neo4j database
+neo4jdb = neo4j.Connector("http://192.168.122.61:7474", ("neo4j", "12345")) # local password of neo4j database
+with open("eot/wowool/samples/cypher-out.cypher", "r") as fh:
    lines = fh.readlines()
-for cypher_query in lines:
-   print(cypher_query)
+for i, cypher_query in enumerate(lines):
+   print(i, cypher_query)
    neo4jdb.run(cypher_query)
