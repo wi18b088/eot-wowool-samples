9fc534055d154313fbbe8af5a2bcbcaf242fc63a
Sjoerd Weigergangs <sjoerdweigergangs@hotmail.com>
2020-12-07 13:31:23 +0100

    all rules

--- eot/wowool/samples/hef-graph-cypher.py
+++ eot/wowool/samples/hef-graph-cypher.py
@@ -53,30 +53,30 @@ graph_config = {
                 "to"        : { "expr" : "EngineType", "label" :"EngineType"},
                 "relation"  : { "label" : "Power" }
           },
-          #{     "from"      : { "expr" : "Reference_Name", "label" :"Reference_Name" }, 
-          #      "to"        : { "expr" : "Reference_Title", "label" :"Reference_Title"  }, 
-          #      "relation"  : { "label" : "AuthorOf" }
-          #}, 
-          #{     "from"      : { "expr" : "Website", "label" :"Website" }, 
-          #      "to"        : { "expr" : "Reference_Name", "label" :"Reference_Name" }, 
-          #      "relation"  : { "label" : "AffiliatedTo" }
-          #},                 
-          #{     "from"      : { "expr" : "Website", "label" :"Website" }, 
-          #      "to"        : { "expr" : "Reference_Title", "label" :"Reference_Title"  }, 
-          #      "relation"  : { "label" : "Source" }
-          #},                 
-          #{     "from"      : { "expr" : "Year", "label" :"Year" }, 
-          #      "to"        : { "expr" : "Reference_Title", "label" :"Reference_Title"  },
-          #      "relation"  : { "label" : "PublishYear" }
-          #},   
-         #{     "from"      : { "expr" : "Flying", "label" :"Flying" }, 
-         #       "to"        : { "expr" : "Article_Title", "label" :"Article_Title"  },
-         #       "relation"  : { "label" : "Found_in" }
-         # },
-         #{     "from"      : { "expr" : "BatteryDict", "label" :"Battery" }, 
-         #       "to"        : { "expr" : "Article_Title", "label" :"Article_Title"  },
-         #       "relation"  : { "label" : "Found_in" }
-         # },                              
+          {     "from"      : { "expr" : "Reference_Name", "label" :"Reference_Name" }, 
+                "to"        : { "expr" : "Reference_Title", "label" :"Reference_Title"  }, 
+                "relation"  : { "label" : "AuthorOf" }
+          }, 
+          {     "from"      : { "expr" : "Website", "label" :"Website" }, 
+                "to"        : { "expr" : "Reference_Name", "label" :"Reference_Name" }, 
+                "relation"  : { "label" : "AffiliatedTo" }
+          },                 
+          {     "from"      : { "expr" : "Website", "label" :"Website" }, 
+                "to"        : { "expr" : "Reference_Title", "label" :"Reference_Title"  }, 
+                "relation"  : { "label" : "Source" }
+          },                 
+          {     "from"      : { "expr" : "Year", "label" :"Year" }, 
+                "to"        : { "expr" : "Reference_Title", "label" :"Reference_Title"  },
+                "relation"  : { "label" : "PublishYear" }
+          },   
+         {     "from"      : { "expr" : "Flying", "label" :"Flying" }, 
+                "to"        : { "expr" : "Article_Title", "label" :"Article_Title"  },
+                "relation"  : { "label" : "Found_in" }
+          },
+         {     "from"      : { "expr" : "BatteryDict", "label" :"Battery" }, 
+                "to"        : { "expr" : "Article_Title", "label" :"Article_Title"  },
+                "relation"  : { "label" : "Found_in" }
+          },                              
 
       ] #Links we can still create: We still have to get the author's of the article it self as well as the title of the article to link those together. 
 }
