20f9187e6a2265295b41c5f091e25a38ae1b72dc
Albert <wi18b088@technikum-wien.at>
2020-12-11 13:48:34 +0100

    Updated startup_fix to add regex rule Doesn't break sentences with abbreviations

--- startup_fix.sh
+++ startup_fix.sh
@@ -10,4 +10,5 @@
 # after starting the container
 
 sed -i 's/link_index = re\.match("\.\*_(\[0-9\])+", uri)\.group(1)/link_index = re\.match("\.\*_(\[0-9\]+)", uri)\.group(1)/' /usr/local/lib/python3.8/dist-packages/eot/wowool/tool/entity_graph/entity_graph.py
+echo "[A-Z]([a-z]){0,5}\." >> /usr/local/lib/python3.8/dist-packages/eot/wowool/package/lxware/english.abbrev
 echo "startup script was executed."
