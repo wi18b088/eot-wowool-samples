730cab17027d8367498ce623b327fccdb129694d
Albert <wi18b088@technikum-wien.at>
2021-01-08 13:52:35 +0100

    Docker changes

--- eot/wowool/samples/CypherToNeo4j.py
+++ eot/wowool/samples/CypherToNeo4j.py
@@ -2,9 +2,12 @@ import neo4j
 import os
 # print(os.listdir())
 neo4jdb = neo4j.Connector("http://localhost:7474", ("neo4j", "12345")) # local password of neo4j database
-#neo4jdb = neo4j.Connector("http://192.168.122.61:7474", ("neo4j", "12345")) # local password of neo4j database
-with open("C:\\Users\\Sjoerd\\whatever\\neo4jgithub\\eot-wowool-samples\\eot\\wowool\\samples\\cypher-out.cypher", "r") as fh:
-#with open("eot/wowool/samples/cypher-out.cypher", "r") as fh:
+# neo4jdb = neo4j.Connector("http://192.168.122.61:7474", ("neo4j", "12345")) # local password of neo4j database
+# with open("C:\\Users\\Sjoerd\\whatever\\neo4jgithub\\eot-wowool-samples\\eot\\wowool\\samples\\cypher-out.cypher", "r") as fh:
+# with open("eot/wowool/samples/cypher-out.cypher", "r") as fh:
+
+# For docker:
+with open("cypher-out.cypher", "r") as fh:
    lines = fh.readlines()
 for i, cypher_query in enumerate(lines):
    print(i, cypher_query)
