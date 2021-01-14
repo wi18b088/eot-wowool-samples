4fa391a2a3e55c8c156c4359aa0082f3e68e502a
Albert <wi18b088@technikum-wien.at>
2021-01-14 15:08:20 +0100

    external inputs and outputs for hef-cypher

--- eot/wowool/samples/hef-graph-cypher.py
+++ eot/wowool/samples/hef-graph-cypher.py
@@ -7,6 +7,7 @@ from eot.wowool.error import Error
 from eot.wowool.tool.entity_graph import EntityGraph
 from eot.io import InputProviders
 import pathlib
+import os
 
 
 # Check if external configuration exists
@@ -16,12 +17,21 @@ external_config_file_path = "/mnt/inout/config/"
 rule_file_name = "HEFrules.dom"
 external_config_file_name = "graphconfig.py"
 
+output_directory = "/mnt/inout/output/hef-graph-cypher/"
+input_docs_directory = "/mnt/inout/output/scraper/springerLink/txt/"
+
+if not os.path.exists(output_directory):
+    pathlib.Path(output_directory).mkdir(parents=True, exist_ok=True)
+
 if pathlib.Path(external_config_file_path + rule_file_name).is_file():
+    print("External domain found")
     myrule = Domain(external_config_file_path + rule_file_name)
 else:
-    myrule = Domain(rule_file_name)
+    print("use default domain")
+    myrule = Domain(os.path.dirname(__file__) + "/" + rule_file_name)
 
 if pathlib.Path( external_config_file_path + external_config_file_name).is_file():
+    print("External config found")
     import importlib.util
     spec = importlib.util.spec_from_file_location("hef-config", external_config_file_path + external_config_file_name)
     config_module = importlib.util.module_from_spec(spec)
@@ -32,15 +42,29 @@ if pathlib.Path( external_config_file_path + external_config_file_name).is_file(
     except AttributeError:
         from graphconfig import graph_config
 else:
+    print("using standard config")
     from graphconfig import graph_config
+
+if os.path.exists(input_docs_directory) and os.path.isdir(input_docs_directory):
+    if not os.listdir(input_docs_directory):
+        # Directory is empty
+        input_provider_path = os.path.dirname(__file__) + "/" + "../../../docs"
+    else:    
+        # Use external input documents
+        print("External documents found")
+        input_provider_path = input_docs_directory
+else:
+    # Directory does not exist
+    print("using default docs")
+    input_provider_path = os.path.dirname(__file__) + "/" + "../../../docs"
 
 try:
     english = Analyzer(language="english")
-    entities = Domain( "english-entity" )
+    entities = Domain("english-entity" )
 
-    f = open("cypher-out.cypher", "w")
-    webpage = open("index.html", "w")
-    for i, ip in enumerate(InputProviders( "../../../docs")):
+    f = open(output_directory + "cypher-out.cypher", "w")
+    webpage = open(output_directory + "index.html", "w")
+    for i, ip in enumerate(InputProviders(input_provider_path)):
         print(f"Processing File no {i}: {ip.id()}")
         doc = english(ip)
         doc = entities(doc)
